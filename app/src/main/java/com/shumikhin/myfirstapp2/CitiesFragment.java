e4318eba8b614a3f48ed337f0dcf306796681b5e
sergey <sashumihin@gmail.com>
2021-04-05 15:40:07 +0300

    6 Урок. Добавл фрагмент.

---
+++ app/src/main/java/com/shumikhin/myfirstapp2/CitiesFragment.java
@@ -0,0 +1,58 @@
+package com.shumikhin.myfirstapp2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CitiesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CitiesFragment extends Fragment {
+
+    // TODO: Rename and change types and number of parameters
+    public static CitiesFragment newInstance(String param1, String param2) {
+        CitiesFragment fragment = new CitiesFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    //Вызывается, когда view фрагмента создан. В качестве
+    //аргумента приходит сам view, у которого можно находить и инициализировать компоненты:
+    //view.findViewById(...).
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cities, container, false);
+    }
+
+    private void initView(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        String[] cities = getResources().getStringArray(R.array.cities);
+        // В этом цикле создаём элемент TextView, заполняем его значениями, и добавляем на экран.
+        // Кроме того, создаём обработку касания на элемент
+        for (String city : cities) {
+            TextView tv = new TextView(getContext());
+            tv.setText(city);
+            tv.setTextSize(30);
+            layoutView.addView(tv);
+        }
+    }
+
+
+}
