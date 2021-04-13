9be3ea42912da836d81ddf1e4affe8f9973e9b2f
sergey <sashumihin@gmail.com>
2021-04-13 17:34:50 +0300

    Урок 8.2 Сделал ресайкл с карточкой и разделителем.

--- app/src/main/java/com/shumikhin/myfirstapp2/ui/SocialNetworkAdapter.java
+++ app/src/main/java/com/shumikhin/myfirstapp2/ui/SocialNetworkAdapter.java
@@ -1,27 +1,34 @@
 package com.shumikhin.myfirstapp2.ui;
 
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.shumikhin.myfirstapp2.R;
+import com.shumikhin.myfirstapp2.data.CardData;
+import com.shumikhin.myfirstapp2.data.CardsSource;
 
 
 //это класс для управления адаптером
 public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdapter.ViewHolder> {
 
-    private String[] dataSource;
     private OnItemClickListener itemClickListener; // Слушатель будет устанавливаться извне
-
+    private static final String TAG = "SocialNetworkAdapter";
+    private CardsSource dataSource;
 
     // Передаём в конструктор источник данных
     // В нашем случае это массив, но может быть и запрос к БД
-    public SocialNetworkAdapter(String[] dataSource) {
+    public SocialNetworkAdapter(CardsSource dataSource) {
         this.dataSource = dataSource;
+        Log.d(TAG, "start");
     }
 
     // Создать новый элемент пользовательского интерфейса
@@ -33,6 +40,7 @@ public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdap
         // Через Inflater
         //viewGroup это контейнер куда мы помещаем наш item
         View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
+        Log.d(TAG, "onCreateViewHolder");
         // Здесь можно установить всякие параметры
         return new ViewHolder(itemView);
     }
@@ -41,31 +49,50 @@ public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdap
     // Вызывается менеджером
     @Override
     public void onBindViewHolder(@NonNull SocialNetworkAdapter.ViewHolder viewHolder, int i) {
+        Log.d(TAG, "onBindViewHolder");
         // Получить элемент из источника данных (БД, интернет...)
         // Вынести на экран, используя ViewHolder
-        viewHolder.getTextView().setText(dataSource[i]); //заносим какой то текст в item. i это позиция в списке.
+        //viewHolder.getTextView().setText(dataSource[i]); //заносим какой то текст в item. i это позиция в списке.
+        viewHolder.setData(dataSource.getCardData(i));
+
+//        if (i%3 == 0 ){
+//            viewHolder.textView.setBackgroundColor(Color.BLUE);
+//        }else {
+//            viewHolder.textView.setBackgroundColor(Color.WHITE);
+//        }
     }
 
     // Вернуть размер данных, вызывается менеджером
     //число итемов не должно быть больше числа строк массива
     @Override
     public int getItemCount() {
-        return dataSource.length;
+        Log.d(TAG, "getItemCount");
+        //return dataSource.length;
+        return dataSource.size();
     }
 
     // Этот класс хранит связь между данными и элементами View
     // Сложные данные могут потребовать несколько View на один пункт списка
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView textView;
+        //private TextView textView;
+        private TextView title;
+        private TextView description;
+        private AppCompatImageView image;
+        private CheckBox like;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = (TextView) itemView;
+            //textView = (TextView) itemView;
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            image = itemView.findViewById(R.id.imageView);
+            like = itemView.findViewById(R.id.like);
+
 
             //Область текста ответственная на обработку нажатий+++++++
             // Обработчик нажатий на этом ViewHolder
-            textView.setOnClickListener(v -> {
+            image.setOnClickListener(v -> {
                 if (itemClickListener != null) {
                     itemClickListener.onItemClick(v, getAdapterPosition());
                 }
@@ -74,9 +101,16 @@ public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdap
 
         }
 
-        public TextView getTextView() {
-            return textView;
+        public void setData(CardData cardData){
+            title.setText(cardData.getTitle());
+            description.setText(cardData.getDescription());
+            like.setChecked(cardData.isLike());
+            image.setImageResource(cardData.getPicture());
         }
+
+        //public TextView getTextView() {
+        //    return textView;
+        //}
 
     }
 
--- app/src/main/java/com/shumikhin/myfirstapp2/ui/SocialNetworkFragment.java
+++ app/src/main/java/com/shumikhin/myfirstapp2/ui/SocialNetworkFragment.java
@@ -1,5 +1,7 @@
 package com.shumikhin.myfirstapp2.ui;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +9,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.shumikhin.myfirstapp2.R;
+import com.shumikhin.myfirstapp2.data.CardsSource;
+import com.shumikhin.myfirstapp2.data.CardsSourceImpl;
 
 public class SocialNetworkFragment extends Fragment {
 
@@ -28,35 +35,61 @@ public class SocialNetworkFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view_lines);
 
         //Заполняем массив из нашего arrays
-        String[] data = getResources().getStringArray(R.array.titles);
+        //String[] data = getResources().getStringArray(R.array.titles);
+        // Получим источник данных для списка
+        CardsSource data = new CardsSourceImpl(getResources()).init();
+
         //Инициализируем менеджер для recyclerView в отдельном методе, так удобней
         initRecyclerView(recyclerView, data);
         return view;
     }
 
-    private void initRecyclerView(RecyclerView recyclerView, String[] data) {
+    private void initRecyclerView(RecyclerView recyclerView, CardsSource data) {
         // Эта установка служит для повышения производительности системы
         recyclerView.setHasFixedSize(true);
 
         // Будем работать со встроенным менеджером
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        //берем не LinearLayoutManager а GridLayoutManager что бы показать как можно при поворте менять количество столбцов
+        int span = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? 3 : 1;
+        LinearLayoutManager layoutManager = new GridLayoutManager(getContext(),span);
+
         recyclerView.setLayoutManager(layoutManager);
         //layoutManager можно задать и в макете строкой как внизу, но это неудобно
         //например - app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager"
 
 
         // Установим адаптер
-        // В адапторе мы заполняем данные из массива
+        // В адапторе (SocialNetworkAdapter) мы заполняем данные из массива
         SocialNetworkAdapter adapter = new SocialNetworkAdapter(data);
 
+        //Устанавливаем адаптер для RecyclerView
         recyclerView.setAdapter(adapter);
+
+
+        // Добавим разделитель карточек
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(),  LinearLayoutManager.VERTICAL);
+
+//        final int version = Build.VERSION.SDK_INT;
+//        if (version >= 21) {
+//            itemDecoration.setDrawable(ResourcesCompat.getDrawable(R.drawable.separator, 0, null));
+//        } else {
+            itemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
+//        }
+        recyclerView.addItemDecoration(itemDecoration);
+
+
+
+
 
         //Область текста ответственная на обработку нажатий+++++++
         // Установим слушателя
         adapter.SetOnItemClickListener(new SocialNetworkAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                Toast.makeText(getContext(), String.format("%s - %d", ((TextView) view).getText(), position), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getContext(), String.format("%s - %d", ((TextView) view).getText(), position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), String.format("Нажали котика - %d", position+1), Toast.LENGTH_SHORT).show();
             }
         });
 
