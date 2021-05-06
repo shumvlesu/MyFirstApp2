0ba7d9a882bc33f1e1faf8f40a0ff2ed578e8d5b
sergey <sashumihin@gmail.com>
2021-05-06 17:55:07 +0300

    11 урок. стр. методички - 14, урок 54:39

--- app/src/main/java/com/shumikhin/myfirstapp2/MainActivity.java
+++ app/src/main/java/com/shumikhin/myfirstapp2/MainActivity.java
@@ -21,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         Button alert3 = findViewById(R.id.alertDialog3);
         alert3.setOnClickListener(clickAlertDialog3);
+
+        Button alertList = findViewById(R.id.alertDialogList);
+        alertList.setOnClickListener(clickAlertDialogList);
+
+        Button alertListSingle = findViewById(R.id.alertDialogListSingle);
+        alertListSingle.setOnClickListener(clickAlertDialogListSingle);
+
+        Button alertMultiList = findViewById(R.id.alertDialogListMulti);
+        alertMultiList.setOnClickListener(clickAlertDialogListMulti);
+
     }
 
     private View.OnClickListener clickAlertDialog1 = new View.OnClickListener() {
@@ -91,6 +101,112 @@ public class MainActivity extends AppCompatActivity {
                                     Toast.makeText(MainActivity.this, "Да!", Toast.LENGTH_SHORT).show();
                                 }
                             });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    };
+
+
+    private View.OnClickListener clickAlertDialogList = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String[] items = getResources().getStringArray(R.array.choose);
+            // Создаём билдер и передаём контекст приложения
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            // В билдере указываем заголовок окна. Можно указывать как ресурс, так и строку)
+            builder.setTitle(R.string.exclamation)
+                    //Вызов метода setItems() в строителе диалога позволяет выбрать пункт из списка. Список в данном
+                    //случае — это массив строк. Одна строка — один пункт.
+                    // Добавим список элементов
+                    .setItems(items, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int item) {
+                            Toast.makeText(MainActivity.this, String.format("Выбран пункт %d", item + 1), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    };
+
+    private int chosen = -1; // Здесь будет храниться выбранный пункт
+
+    private View.OnClickListener clickAlertDialogListSingle = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final String[] items = getResources().getStringArray(R.array.choose);
+            // Создаём билдер и передаём контекст приложения
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            // В билдере указываем заголовок окна. Можно указывать как ресурс,
+            //так и строку
+            builder.setTitle(R.string.exclamation)
+                    // Добавляем список элементов; chosen — выбранный элемент,
+                    // если = -1, то ни один не выбран
+                    .setSingleChoiceItems(items, chosen, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int item) {
+                            chosen = item; // Обновляем выбранный элемент
+                        }
+                    })
+                    .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "Отмена!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setPositiveButton("Ок", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if (chosen == -1) {
+                                Toast.makeText(MainActivity.this, "Ок, пункт не выбран !", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            Toast.makeText(MainActivity.this, String.format("Ок, выбран '%s' !", items[chosen]), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    };
+
+    private View.OnClickListener clickAlertDialogListMulti = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final String[] items = getResources().getStringArray(R.array.choose);
+            final boolean[] chosen = {false, true, false};
+            // Создаём билдер и передаём контекст приложения
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            // В билдере указываем заголовок окна (можно указывать как ресурс,
+            // так и строку)
+            builder.setTitle(R.string.exclamation)
+                    // Добавляем список элементов; булев chosen — массив
+                    // с выбранными элементами
+                    .setMultiChoiceItems(items, chosen, new DialogInterface.OnMultiChoiceClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                                    chosen[i] = b; // При переключении обновляем ячейку в массиве
+                                }
+                            })
+                    .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this, "Отмена!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setPositiveButton("Ок", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Собираем выбранные элементы в строку
+                            StringBuilder sb = new StringBuilder("Ок, выбрано: ");
+                            for (int index = 0; index < chosen.length; index++) {
+                                if (chosen[index]) {
+                                    sb.append(items[index]);
+                                    sb.append("; ");
+                                }
+                            }
+                            Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
             AlertDialog alert = builder.create();
             alert.show();
         }
